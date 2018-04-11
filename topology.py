b96a73662130e107f909ac791c3d827fc5df92d4
Christian Franke <nobody@nowhere.ws>
2018-04-11 22:11:46 +0200

    Use ip numbering that makes traceroutes easier to read

--- topology.py
+++ topology.py
@@ -6,9 +6,11 @@ from config import topo_width as width
 from config import topo_height as height
 
 namespaces = []
+xy = {}
 for x in range(width):
     for y in range(height):
         namespaces.append('rt-%d-%d' % (x,y))
+        xy[namespaces[-1]] = (x,y)
 
 links = []
 for y in range(height - 1):
@@ -35,8 +37,8 @@ def config_for(ns, daemon):
 
 for idx,ns in enumerate(namespaces):
     configs[ns] = {
-            'loopback_v4': ipaddress.IPv4Address('100.0.0.1') + idx,
-            'loopback_v6': ipaddress.IPv6Address('2001:db8:64::1') + idx,
+            'loopback_v4': ipaddress.IPv4Address('100.0.%d.%d' % xy[ns]),
+            'loopback_v6': ipaddress.IPv6Address('2001:db8:64::%x:%x' % xy[ns]),
             'net': '49.0000.0000.%04x.00' % (idx + 1),
             'links': sorted([ link[1] for link in links if link[0] == ns ]
                           + [ link[0] for link in links if link[1] == ns ]),
